9488e7ed299c555203c76ed6e3913e636cb01451
Ryan Ghaida <ryaboug@gmail.com>
2018-04-24 17:44:29 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(str_arr)
+  str_arr.sort_by {|word| word.length}
+end
+
+def swap_elements(array)
+  
+end
