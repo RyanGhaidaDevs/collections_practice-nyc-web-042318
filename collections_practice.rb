66e800e73d50d80830b7a5201a0748668369f37d
Ryan Ghaida <ryaboug@gmail.com>
2018-04-27 19:11:09 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,57 @@
 
+def sort_array_asc(array)
+  array.sort do |a, b|
+    #compares two elemetns of array at a time, if a < b then leave alone if b > a, swap
+    a<=>b
+  end
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+      b<=>a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length<=>b.length
+  end
+end
+
+def swap_elements(array)
+array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |name|
+  name[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select do |name|
+    name.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  x = 0
+  array.each do |number|
+    x = x + number
+  end
+  return x
+end
+
+def add_s(array)
+  array.map do |word|
+    if word == array[1]
+      "#{word}"
+    else
+      "#{word}s"
+    end
+  end
+end
